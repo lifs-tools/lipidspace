3be4035daa7a34ee550f0e0b906268e65e369b28
dominik.kopczynski <dominik.kopczynski@isas.de>
2019-12-20 15:28:30 +0100

    more and more functions

---
+++ src/domain/StructuralFattyAcid.h
@@ -0,0 +1,17 @@
+#ifndef STRUCTURAL_FATTY_ACID_H
+#define STRUCTURAL_FATTY_ACID_H
+
+#include "MolecularFattyAcid.h"
+#include <string>
+#include "LipidExceptions.h"
+#include "LipidEnums.h"
+
+using namespace std;
+
+class StructuralFattyAcid : MolecularFattyAcid {
+
+public:
+    StructuralFattyAcid(string name, int num_carbon, int num_double_bonds, int num_hydroxyl, LipidFaBondType lipid_FA_bond_type, bool lcb, int position);
+};            
+
+#endif /* STRUCTURAL_FATTY_ACID_H */
