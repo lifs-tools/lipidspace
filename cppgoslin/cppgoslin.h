b934a6d160fbe6d639400db3c988a60386518721
dominik.kopczynski <dominik.kopczynski@isas.de>
2020-01-07 14:29:36 +0100

    adding global header file

---
+++ cppgoslin/cppgoslin.h
@@ -0,0 +1,26 @@
+#ifndef CPPGOSLIN_H
+#define CPPGOSLIN_H
+
+#include "cppgoslin/domain/Adduct.h"
+#include "cppgoslin/domain/FattyAcid.h"
+#include "cppgoslin/domain/Fragment.h"
+#include "cppgoslin/domain/IsomericFattyAcid.h"
+#include "cppgoslin/domain/LipidAdduct.h"
+#include "cppgoslin/domain/LipidEnums.h"
+#include "cppgoslin/domain/LipidExceptions.h"
+#include "cppgoslin/domain/LipidIsomericSubspecies.h"
+#include "cppgoslin/domain/LipidMolecularSubspecies.h"
+#include "cppgoslin/domain/LipidSpecies.h"
+#include "cppgoslin/domain/LipidSpeciesInfo.h"
+#include "cppgoslin/domain/LipidStructuralSubspecies.h"
+#include "cppgoslin/domain/MolecularFattyAcid.h"
+#include "cppgoslin/domain/StructuralFattyAcid.h"
+#include "cppgoslin/parser/BaseParserEventHandler.h"
+#include "cppgoslin/parser/GoslinFragmentParserEventHandler.h"
+#include "cppgoslin/parser/GoslinParserEventHandler.h"
+#include "cppgoslin/parser/KnownGrammars.h"
+#include "cppgoslin/parser/KnownParsers.h"
+#include "cppgoslin/parser/LipidMapsParserEventHandler.h"
+#include "cppgoslin/parser/Parser.h"
+
+#endif /* CPPGOSLIN_H */
