f9ab202ab331edf7bc06aa9895f8eae8dd51b434
dominik.kopczynski <dominik.kopczynski@isas.de>
2020-02-25 09:35:00 +0100

    adding missing source file for tests

---
+++ cppgoslin/tests/SwissLipidsTest.cpp
@@ -0,0 +1,50 @@
+#include "cppgoslin/cppgoslin.h"
+#include <set>
+#include <string>
+#include <stdlib.h>
+#include <iostream>
+#include <cassert>
+#include <fstream>
+
+
+using namespace std;
+
+int main(int argc, char** argv){
+    
+    try {
+        LipidAdduct* lipid;
+        SwissLipidsParser swiss_lipids_parser;
+        
+        
+        
+        // test several more lipid names
+        vector<string> lipid_names;
+        ifstream infile("cppgoslin/tests/swiss-lipids-test.csv");
+        string line;
+        while (getline(infile, line)){
+            line = strip(line, ' ');
+            lipid_names.push_back(line);
+        }
+        infile.close();
+        
+        for (auto lipid_name : lipid_names){
+            lipid = swiss_lipids_parser.parse(lipid_name);
+            if (lipid == NULL){
+                throw LipidException("Error: '" + lipid_name + "'");
+            }
+            else {
+                delete lipid;
+            }
+        }
+        
+        
+        cout << "All tests passed without any problem" << endl;
+
+    }
+    catch (LipidException &e){
+        cout << "Exception:" << endl;
+        cout << e.what() << endl;
+    }
+    
+    return EXIT_SUCCESS;
+}
