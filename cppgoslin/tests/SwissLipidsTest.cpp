a83b0ab94c96808aa1e59053fa89eee103098caa
Dominik Kopczynski <dominik.kopczynski@tu-dortmund.de>
2021-09-14 17:50:09 +0200

    added support for furans in HMDB parser

--- cppgoslin/tests/SwissLipidsTest.cpp
+++ cppgoslin/tests/SwissLipidsTest.cpp
@@ -53,15 +53,8 @@ int main(int argc, char** argv){
     infile.close();
     
     for (auto &lipid_name : lipid_names){
-        try {
-            lipid = parser.parse(lipid_name);
-            assert(lipid != NULL);
-            delete lipid;
-        }
-        catch (LipidException &e){
-            cout << "Exception: " << lipid_name << endl;
-            cout << e.what() << endl;
-        }
+        lipid = parser.parse(lipid_name);
+        assert(lipid != 0);
     }
     
     
