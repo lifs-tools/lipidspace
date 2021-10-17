4cac708163162e823059251be47049b82e7c1423
Dominik Kopczynski <dominik.kopczynski@tu-dortmund.de>
2021-10-17 14:44:01 +0200

    more and more translation

--- src/lipidspace.cpp
+++ src/lipidspace.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <string>
 #include <cppgoslin/cppgoslin.h>
+#include <math.h>
  
 using namespace std;
 using namespace Eigen;
@@ -25,17 +26,77 @@ class LipidSpace {
 public:
     LipidParser parser;
     map<string, int*> class_matrix;
+    
+    LipidSpace();
     Table* create_Table(string lipid_list_file);
     ArrayXd compute_PCA_variances(MatrixXd m);
     MatrixXd compute_PCA(MatrixXd m);
     void lipid_similarity(LipidAdduct* l1, LipidAdduct* l2, int& union_num, int& inter_num);
+    void fatty_acyl_similarity(FattyAcid* f1, FattyAcid* f2, int& union_num, int& inter_num);
 };
 
 
 
-void LipidSpace::lipid_similarity(LipidAdduct* l1, LipidAdduct* l2, int& union_num, int& inter_num){
-    inter_num = 1;
-    union_num = 2;
+LipidSpace::LipidSpace(){
+    // load precomputed class distance matrix
+    ifstream infile("data/classes-matrix.csv");
+    if (!infile.good()){
+        cout << "Error: file 'data/classes-matrix.csv' not found." << endl;
+        exit(-1);
+    }
+    string line;
+    
+    while (getline(infile, line)){
+        if (line.length() == 0) continue;
+        vector<string>* tokens = goslin::split_string(line, '\t', '"');
+        
+        int* values = new int[2];
+        string key = tokens->at(0) + "/" + tokens->at(1);
+        values[0] = abs(atoi(tokens->at(2).c_str()));
+        values[1] = abs(atoi(tokens->at(3).c_str()));
+        class_matrix.insert({key, values});
+        delete tokens;
+    }
+}
+
+
+void LipidSpace::fatty_acyl_similarity(FattyAcid* f1, FattyAcid* f2, int& union_num, int& inter_num){
+    
+}
+    
+
+void LipidSpace::lipid_similarity(LipidAdduct* lipid1, LipidAdduct* lipid2, int& union_num, int& inter_num){
+    string key = lipid1->get_extended_class() + "/" + lipid2->get_extended_class();
+    union_num = class_matrix.at(key)[0];
+    inter_num = class_matrix.at(key)[1];
+    
+    int l = min(lipid1->lipid->fa_list.size(), lipid2->lipid->fa_list.size());
+    for (int i = 0; i < l; ++i){
+        int un, in;
+        fatty_acyl_similarity(lipid1->lipid->fa_list.at(i), lipid2->lipid->fa_list.at(i), un, in);
+        union_num += un;
+        inter_num += in;
+    }
+      
+      
+    vector<FattyAcid*>* fa_list_1 = &lipid1->lipid->fa_list;
+    vector<FattyAcid*>* fa_list_2 = &lipid2->lipid->fa_list;
+    if (fa_list_1->size() < fa_list_2->size()){
+        fa_list_1 = &lipid2->lipid->fa_list;
+        fa_list_2 = &lipid1->lipid->fa_list;
+    }
+        
+    if (fa_list_1->size() > fa_list_2->size()){
+        for (int i = fa_list_2->size(); i < fa_list_1->size(); ++i){
+            FattyAcid* fa = fa_list_1->at(i);
+            union_num += fa->get_double_bonds();
+            ElementTable* e = fa->get_elements();
+            for (auto kv : *e){
+                if (kv.first != ELEMENT_H) union_num += kv.second;
+            }
+            delete e;
+        }
+    }
 }
 
 
@@ -142,7 +203,7 @@ int main(int argc, char** argv) {
     LipidSpace lipid_space;
     Table* table = lipid_space.create_Table(input_lists.at(0));
     
-    cout << table->m << endl;
+    for (auto kv : lipid_space.class_matrix) cout << kv.first << " -> (" << kv.second[0] << " / " << kv.second[1] << endl;
     delete table;
     
 }
