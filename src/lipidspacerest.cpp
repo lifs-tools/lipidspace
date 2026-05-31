508dc5d83b55c6117adb0ce1aecac377e509b4ac
Nils Hoffmann <3309580+nilshoffmann@users.noreply.github.com>
2026-05-30 21:49:46 -0700

    Removed logging handler registration

--- src/lipidspacerest.cpp
+++ src/lipidspacerest.cpp
@@ -567,23 +567,7 @@ int main(int argc, char *argv[])
 
     QCoreApplication app(argc, argv);
     QCoreApplication::setApplicationName("LipidSpaceREST");
-    QCoreApplication::setApplicationVersion(GlobalData::LipidSpace_version.c_str());// Register Qt6 message handler immediately after creating QCoreApplication
-    
-    // Register Qt6 message handler immediately after creating QCoreApplication
-    qInstallMessageHandler([](QtMsgType msgType, const QMessageLogContext &context,
-                            const QString &message) {
-        // QtInfoMsg=4, QtDebugMsg=0, QtWarningMsg=1, QtCriticalMsg=2, QtFatalMsg=3
-        const char* level = (msgType == QtInfoMsg)     ? "INFO"  :
-                            (msgType == QtDebugMsg)    ? "DEBUG" :
-                            (msgType == QtWarningMsg)  ? "WARN"  : "ERROR";
-        // context.function is nullptr in release builds (QT_NO_MESSAGELOGCONTEXT)
-        if (context.function)
-            std::cerr << "[Qt-" << level << "] [" << context.function << "] ";
-        else
-            std::cerr << "[Qt-" << level << "] ";
-        std::cerr << message.toStdString() << "\n";
-        std::cerr.flush();
-    });
+    QCoreApplication::setApplicationVersion(GlobalData::LipidSpace_version.c_str());
 
     QString host = "0.0.0.0";
     QString tmp_folder = ".";
